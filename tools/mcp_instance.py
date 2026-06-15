27a86b3fa3cf9792d5320da2066651b75289f9a8
Manish Yadav <ymanish@ariqt.com>
2026-06-15 18:33:57 +0530

    fix scopes

--- tools/mcp_instance.py
+++ tools/mcp_instance.py
@@ -15,7 +15,7 @@ auth_provider = AzureProvider(
     client_secret=client_secret,
     tenant_id=tenant_id,
     base_url=base_url,
-    required_scopes="api://8601532b-6517-43c9-a321-ed5f6f0b8a97/Read",
+    required_scopes=["api://8601532b-6517-43c9-a321-ed5f6f0b8a97/Read"],
     jwt_signing_key=jwt_signing_key,
 )
 
